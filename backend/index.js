5654283b79733f91a1aa89419397d40ec4f52bf1
JitendraTmg <jitendratamang554@gmail.com>
2026-05-18 22:33:56 +0545

    first section

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+import express from 'express';
+import connectDB from './database/db.js';
+import authRoute from './routes/authRoute.js';
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+//middleware
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/api/auth',authRoute)
+
+//http:localhost:8000/api/auth/google
+
+app.listen(PORT,()=>{
+    connectDB()
+    console.log(`Server is listening at port:${PORT}`);
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Pricing from "./pages/Pricing"
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/pricing' element={<Pricing/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
